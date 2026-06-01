a9079dbc48761e7d992bba9c72424a45e3f2da1d
ALex <sasha.hivuk@gmail.com>
2026-06-01 14:04:14 +0300

    Museum2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,30 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const asideMenu = document.querySelector('.aside');
+  const burgerBtn = document.querySelector('.top-bar__menu-btn');
+  const asideBtn = document.querySelector('.aside__menu-btn');
+
+  if (asideBtn && asideMenu && burgerBtn) {
+    burgerBtn.addEventListener('click', () => {
+      asideMenu.classList.add('aside--open');
+      document.body.style.overflow = 'hidden';
+    });
+
+    asideBtn.addEventListener('click', () => {
+      asideMenu.classList.remove('aside--open');
+      document.body.style.overflow = '';
+    });
+  }
+
+  const asideMenuLinks = document.querySelectorAll('.nav__link');
+
+  asideMenuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      if (asideMenu) {
+        asideMenu.classList.remove('aside--open');
+      }
+      document.body.style.overflow = '';
+    });
+  });
+});
